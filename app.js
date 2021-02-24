dc4e56721e3b293e8ef4aa566a9978b723d3f9e0
djneces <j.necesanek@hotmail.com>
2021-02-24 12:44:45 +0100

    Add start script

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ app.use((err, req, res, next) => {
 })
 
 // ***** LISTEN ****
-
-app.listen(3000, () => {
-    console.log('Serving at 3000');
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Serving at ${port}`);
 })
